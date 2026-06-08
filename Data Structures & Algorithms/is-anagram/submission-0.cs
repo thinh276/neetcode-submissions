43fb8b377431480f4519fcfb74cccb4e877f9894
Thinh Phan <79763166+thinh276@users.noreply.github.com>
2026-06-08 16:44:32 +0900

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length){
+            return false;
+        }
+        
+        var counts = new Dictionary<char, int>();
+        foreach (char c in s){
+            if (!counts.ContainsKey(c)) counts[c] = 0;
+            counts[c]++;
+        }        
+        foreach (char c in t){
+            if (!counts.ContainsKey(c)) return false;
+            counts[c]--;
+            if (counts[c] < 0) return false;
+        }   
+        return true;
+    }
+}
